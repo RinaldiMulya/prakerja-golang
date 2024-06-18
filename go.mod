69f5a55f38d2b5848bd6f96088bc478f097fd9fb
Rinaldi Mulya  Pratama <105484093+RinaldiMulya@users.noreply.github.com>
2024-06-18 21:36:20 +0700

    Tugas Pertemuan 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module prakerja-golang-Hacktiv8
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var firstName = "John"
+	// fmt.Println(firstName)
+
+    // fmt.Println("Hello, World!")
+    // var age8 uint8 = 200
+    // fmt.Println("age8, type:NT value:Nv", age8, age8)
+    // bool1 := true
+    // bool2 := false
+    // fmt.Println("bool1, type:NT value:Nv", bool1, bool1)
+    // fmt.Println("bool2, type:NT value:Nv", bool2, bool2)
+
+	// var iface1 interface{}
+	// iface1 = 100
+	// fmt.Println("iface1, type:NT value:Nv", iface1, iface1)
+	// iface1 = "hello string"
+	// fmt.Println("iface1, type:NT value:Nv", iface1, iface1)
+
+	var namaAwal string = "Rinaldi" // type String
+	var age int =23
+	var isfalse bool = false
+	fmt.Printf("%s\n", namaAwal)
+    fmt.Printf("%d\n", age)
+    fmt.Printf("%t\n", isfalse)
+	fmt.Printf("String\n")
+    fmt.Printf("integer\n")
+    fmt.Printf("Boolean\n")
+
+}
